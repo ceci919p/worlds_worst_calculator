fd8fd17bf25a4ba082ea834060ed051ea24df415
Cecilie Jasmin Jørgensen <ceci919p@stud.kea.dk>
2022-02-04 15:07:32 +0100

    function clickCalculate

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 "use strict";
 
+let result;
+let resultValue;
+let operator;
+let rounding;
+
 const calculateButton = document.querySelector("#calculate");
+const firstNumber = document.querySelector("#firstnumber");
+const secondNumber = document.querySelector("#secondnumber");
+const results = document.querySelector("#results");
+const checkbox = document.querySelector("#doround");
 
 document.addEventListener("DOMContentLoaded", userSetup);
 
@@ -17,4 +26,32 @@ function clear() {
 
 function clickCalculate() {
   console.log("clicked");
+  //operator er der vi plusser, ganger m.m.
+  operator = document.querySelector("#operator").value;
+  if (operator === "add") {
+    resultValue = parseInt(firstNumber.value) + parseInt(secondNumber.value);
+  } else if (operator === "sub") {
+    resultValue = firstNumber.value - secondNumber.value;
+  } else if (operator === "mul") {
+    resultValue = firstNumber.value * secondNumber.value;
+  } else if (operator === "div") {
+    resultValue = firstNumber.value / secondNumber.value;
+  }
+  console.log("read");
+
+  //tjekker om checkbox er valgt
+  if (checkbox.checked === true) {
+    //Tjek hvor mange decimaler
+    rounding = parseInt(document.querySelector("#decimals").value);
+    //rund op
+    resultValue = resultValue.toFixed(rounding);
+  }
+
+  //Få resultat til at stå i første nummer
+  firstNumber.value = resultValue;
+  //Tilføj til liste med resultater med scrol
+  result = document.createElement("li");
+  result.textContent = resultValue;
+  results.appendChild(result);
+  results.scrollTop = results.scrollHeight;
 }
