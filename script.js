0865c0994744843d31d3677bde8eaca9ef4fa0a1
Cecilie Jasmin Jørgensen <ceci919p@stud.kea.dk>
2022-02-04 14:59:03 +0100

    clear function

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 "use strict";
 
+const calculateButton = document.querySelector("#calculate");
+
 document.addEventListener("DOMContentLoaded", userSetup);
 
 function userSetup() {
   document.querySelector("#clear").addEventListener("click", clear);
   console.log("test");
+  calculateButton.addEventListener("click", clickCalculate);
+}
+
+function clear() {
+  //clear resultat liste
+  results.innerHTML = "";
+}
+
+function clickCalculate() {
+  console.log("clicked");
 }
