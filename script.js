4538890d506141e89cd910996c9062e041779530
Cecilie Jasmin Jørgensen <ceci919p@stud.kea.dk>
2022-02-04 14:54:07 +0100

    userSetup

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 "use strict";
+
+document.addEventListener("DOMContentLoaded", userSetup);
+
+function userSetup() {
+  document.querySelector("#clear").addEventListener("click", clear);
+  console.log("test");
+}
